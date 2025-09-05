4e0fa315154ccc1c7653827b3483a2cf30bd9cf9
NyawToo <julianandresperez904@hotmail.com>
2025-09-04 23:55:21 -0500

    Modificacion Data Base

---
+++ src/models/Personal.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Personal = sequelize.define('Personal', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cargo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  especializacion: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  anosExperiencia: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  foto: {
+    type: DataTypes.STRING(500),
+    allowNull: true
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = Personal;
